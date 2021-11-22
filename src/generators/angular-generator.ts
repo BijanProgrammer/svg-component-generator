e78fc46ec2bd4fd91b780e92d4d6674e6535f596
BijanProgrammer <bijaneisapour@gmail.com>
2021-11-22 23:31:27 +0330

    [ADD] illustrations

--- src/generators/angular-generator.ts
+++ src/generators/angular-generator.ts
@@ -1,25 +1,25 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-import {Icon} from '../models/icon';
+import {Svg} from '../models/svg';
 import {titleCase} from '../utils/string.utils';
 
-const generateComponentContent = (name: string): string => {
+const generateComponentContent = (prefix: string, name: string): string => {
     return `import {Component} from '@angular/core';
 
 @Component({
-    selector: 'icon-${name}',
+    selector: '${prefix}-${name}',
     templateUrl: './${name}.component.svg',
 })
 export class ${titleCase(name)}Component {}
 `;
 };
 
-const generateModuleContent = (icons: Icon[]): string => {
-    const components = icons.map((x) => `${titleCase(x.name)}Component`);
+const generateModuleContent = (moduleName: string, svgs: Svg[]): string => {
+    const components = svgs.map((x) => `${titleCase(x.name)}Component`);
 
     const declarations = components.map((x) => `        ${x},`);
-    const imports = components.map((x, i) => `import {${x}} from './${icons[i].name}/${icons[i].name}.component';`);
+    const imports = components.map((x, i) => `import {${x}} from './${svgs[i].name}/${svgs[i].name}.component';`);
 
     return `import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
@@ -35,35 +35,68 @@ ${declarations.join('\n')}
 ${declarations.join('\n')}
     ],
 })
-export class IconsModule {}
+export class ${moduleName} {}
 `;
 };
 
-export const generateAngularComponents = async (outputDirectory: string, icons: Icon[]): Promise<void> => {
+const generateComponents = async (
+    outputDirectory: string,
+    svgs: Svg[],
+    prefix: string,
+    moduleFilename: string,
+    moduleName: string
+): Promise<Promise<any>[]> => {
     const promises: Promise<any>[] = [];
 
-    for (const icon of icons) {
-        console.log(`generating ${icon.name} files ...`);
+    for (const svg of svgs) {
+        console.log(`generating ${svg.name} files ...`);
 
-        await fs.promises.mkdir(path.join(outputDirectory, icon.name), {recursive: true});
+        await fs.promises.mkdir(path.join(outputDirectory, svg.name), {recursive: true});
 
         const svgFilePromise = fs.promises.writeFile(
-            path.join(outputDirectory, icon.name, `${icon.name}.component.svg`),
-            icon.content
+            path.join(outputDirectory, svg.name, `${svg.name}.component.svg`),
+            svg.content
         );
 
         const componentFilePromise = fs.promises.writeFile(
-            path.join(outputDirectory, icon.name, `${icon.name}.component.ts`),
-            generateComponentContent(icon.name)
+            path.join(outputDirectory, svg.name, `${svg.name}.component.ts`),
+            generateComponentContent(prefix, svg.name)
         );
 
         promises.push(svgFilePromise, componentFilePromise);
     }
 
     const moduleFilePromise = fs.promises.writeFile(
-        path.join(outputDirectory, 'icons.module.ts'),
-        generateModuleContent(icons)
+        path.join(outputDirectory, moduleFilename),
+        generateModuleContent(moduleName, svgs)
     );
 
-    await Promise.all([...promises, moduleFilePromise]);
+    return [...promises, moduleFilePromise];
+};
+
+export const generateAngularComponents = async (
+    outputDirectory: string,
+    icons: Svg[],
+    illustrations: Svg[]
+): Promise<void> => {
+    console.log('removing components folder ...');
+    await fs.promises.rmdir(outputDirectory, {recursive: true});
+
+    const iconPromises = await generateComponents(
+        path.join(outputDirectory, 'icons'),
+        icons,
+        'icon',
+        'icons.module.ts',
+        'IconsModule'
+    );
+
+    const illustrationPromises = await generateComponents(
+        path.join(outputDirectory, 'illustrations'),
+        illustrations,
+        'illustration',
+        'illustrations.module.ts',
+        'IllustrationsModule'
+    );
+
+    await Promise.all([...iconPromises, ...illustrationPromises]);
 };
--- src/main.ts
+++ src/main.ts
@@ -3,37 +3,45 @@ import * as path from 'path';
 
 import {generateAngularComponents} from './generators/angular-generator';
 
-import {Icon} from './models/icon';
+import {Svg} from './models/svg';
 
 import {printSeparator} from './utils/console.utils';
 
 const ICONS_PATH = './assets/icons/';
+const ILLUSTRATIONS_PATH = './assets/illustrations/';
 const ANGULAR_COMPONENTS_PATH = './components/angular/';
 
-const icons: Icon[] = [];
+let icons: Svg[] = [];
+let illustrations: Svg[] = [];
 
-const loadIcons = async (): Promise<void> => {
-    const filenames = await fs.promises.readdir(ICONS_PATH);
+const loadSvgs = async (svgsPath: string): Promise<Svg[]> => {
+    const filenames = await fs.promises.readdir(svgsPath);
+    const svgs: Svg[] = [];
 
     for (const filename of filenames) {
-        const fullPath = path.join(ICONS_PATH, filename);
+        const fullPath = path.join(svgsPath, filename);
 
         const {name, ext: extension} = path.parse(fullPath);
-        if (extension !== '.svg') return;
+        if (extension !== '.svg') continue;
 
         const content = await fs.promises.readFile(fullPath, 'utf8');
-        icons.push({name, content});
+        svgs.push({name, content});
     }
+
+    return svgs;
 };
 
 const main = async (): Promise<void> => {
     console.log('loading icons ...');
-    await loadIcons();
+    icons = await loadSvgs(ICONS_PATH);
+
+    console.log('loading illustrations ...');
+    illustrations = await loadSvgs(ILLUSTRATIONS_PATH);
 
     printSeparator();
 
     console.log('generating angular components ...');
-    await generateAngularComponents(ANGULAR_COMPONENTS_PATH, icons);
+    await generateAngularComponents(ANGULAR_COMPONENTS_PATH, icons, illustrations);
 };
 
 main().then(() => console.info('Done!'));
